47c3a11dcb29d636b23c620c072a1d336715be83
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2024-01-17 14:58:01 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@ window.onload = function(){
 var lang = ['en','ru'];
 var lang_id = localStorage.getItem("lang_id");
 var host_len = 0;
-var themeButton = document.querySelector('.main__rueng');
 var different_directories = false;
 var dir = '/';
 
@@ -10,28 +9,9 @@ if(different_directories === true){
 if(lang_id === null){
 lang_id = 0;
 }
-document.getElementById('main__rueng').onclick = changeURLLanguage;
+document.getElementById('main-languages').onclick = changeURLLanguage;
 document.getElementById('main__rueng-mobile').onclick = changeURLLanguage;
 document.getElementById(lang[lang_id]).classList.add('active-lang');
-
-function changeURLLanguage() {
-    pr_lang_id = lang_id;
-if(event.target.id === 'ru'){
-    lang_id = 1;
-}else{
-    lang_id = 0;
-}
-localStorage.setItem("lang_id", lang_id);
-    if(pr_lang_id != lang_id){
-    if(lang_id === 0){
-        document.location.href = document.location.href.replace(dir + lang[lang_id+1],"")
-    }else{
-        host_len = '/index.html'.length;
-        pathname_len = window.location.pathname.length;
-        document.location.href = window.location.pathname.substring(0, pathname_len - host_len) + dir + lang[lang_id] + '/index.html'
-    }
-}
-}
 }
 
 
@@ -54,3 +34,21 @@ function linkClickHandler(){
 }
 }
 
+function changeURLLanguage() {
+    pr_lang_id = lang_id;
+if(event.target.id === 'ru'){
+    lang_id = 1;
+}else{
+    lang_id = 0;
+}
+localStorage.setItem("lang_id", lang_id);
+    if(pr_lang_id != lang_id){
+    if(lang_id === 0){
+        document.location.href = document.location.href.replace(dir + lang[lang_id+1],"")
+    }else{
+        host_len = '/index.html'.length;
+        pathname_len = window.location.pathname.length;
+        document.location.href = window.location.pathname.substring(0, pathname_len - host_len) + dir + lang[lang_id] + '/index.html'
+    }
+}
+}
