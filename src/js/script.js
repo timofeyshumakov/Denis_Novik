f7c95515e71f457f4249568015d5b69f21d4d020
timofeyshumakov <136724219+timofeyshumakov@users.noreply.github.com>
2024-02-06 20:31:43 +0300

    Delete src directory

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-let links = document.querySelectorAll('.menu-item > a');
-links.forEach(item => {
-    item.addEventListener('click', linkClickHandler)
-})
-function linkClickHandler(){
-    document.getElementById(this.getAttribute('data-link')).scrollIntoView({behavior: "smooth" });
-}
-}
